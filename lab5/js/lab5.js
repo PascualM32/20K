b09006ccaf9adf60cf9390abcd28aae4a2f59568
PascualM32 <91556503+PascualM32@users.noreply.github.com>
2021-10-22 17:35:44 -0700

    Creation of Lab 5
    This is my work for lab lab 5. Second time working with JavaScript. , Worked with Data Types & Variables in Javascript. Created a webpage in .html and styled it with .css.

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/**
+ * Author:    Pascual Martinez
+ * Created:   10.19.2021
+ * Lisence: Public Domain.
+ **/
+
+// Define Variables
+ var make = "volkswagen";
+ var model = "golf";
+ var color = "navy";
+ var year = "2019";
+
+//calculate
+ age = 2021 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
