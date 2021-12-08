6fe9874bb5c7821ec700bb08a55d8d1ed2630dac
PascualM32 <91556503+PascualM32@users.noreply.github.com>
2021-12-07 19:36:16 -0800

    Lab 11 finish

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,5 +1,26 @@
 /**
  * Author:    Pascual Martinez
- * Created:   10.20.2021
+ * Created:   11.09.2021
  * Lisence: Public Domain.
  **/
+
+// Make a button for each section
+$("#challenge").append("<button id='c-button'>Click Me</button>");
+$("#problem").append("<button id='p-button'>Click Me</button>");
+$("#results").append("<button id='r-button'>Click Me</button>");
+
+// Add a function to each
+$("#c-button").click(function(){
+  // find the section, toggle a class
+  $(this).parent().toggleClass("special");
+})
+
+$("#p-button").click(function(){
+  // find the section, toggle a class
+  $(this).parent().toggleClass("special");
+})
+
+$("#r-button").click(function(){
+  // find the section, toggle a class
+  $(this).parent().toggleClass("special");
+})
