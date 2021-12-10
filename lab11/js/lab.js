2818ebec4951807119d65f05da221061804e452f
PascualM32 <91556503+PascualM32@users.noreply.github.com>
2021-12-10 12:16:48 -0800

    Lab 11 fixes

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -4,23 +4,23 @@
  * Lisence: Public Domain.
  **/
 
-// Make a button for each section
+// Make a button for each section, challenge, problems, and results
 $("#challenge").append("<button id='c-button'>Click Me</button>");
 $("#problem").append("<button id='p-button'>Click Me</button>");
 $("#results").append("<button id='r-button'>Click Me</button>");
 
-// Add a function to each
+// Add a function to each button in order to change background color
 $("#c-button").click(function(){
-  // find the section, toggle a class
+  // find the specific section and toggle the class "special" accordingly
   $(this).parent().toggleClass("special");
 })
 
 $("#p-button").click(function(){
-  // find the section, toggle a class
+  // find the specific section and toggle the class "special" accordingly
   $(this).parent().toggleClass("special");
 })
 
 $("#r-button").click(function(){
-  // find the section, toggle a class
+  // find the specific section and toggle the class "special" accordingly
   $(this).parent().toggleClass("special");
 })
